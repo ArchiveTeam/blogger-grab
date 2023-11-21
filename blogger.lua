108aa9b0ea3e9e2a6911c7f24d2dcd623fff621f
arkiver <arkiver@protonmail.com>
2023-11-21 23:37:12 +0100

    Version 20231121.11. Fix handling https:///.

--- blogger.lua
+++ blogger.lua
@@ -308,7 +308,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
   end
 
   local function fix_case(newurl)
-    if not string.match(newurl, "^https?://.") then
+    if not string.match(newurl, "^https?://[^/]") then
       return newurl
     end
     if string.match(newurl, "^https?://[^/]+$") then
