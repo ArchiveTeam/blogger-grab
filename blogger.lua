c06375d9859cde09ddcdfb1c8178421a12753750
arkiver <arkiver@protonmail.com>
2023-11-23 01:11:58 +0100

    Version 20231123.01. Do not do retries on url:* item.

--- blogger.lua
+++ blogger.lua
@@ -633,6 +633,9 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
     io.stdout:write("Server returned bad response. ")
     io.stdout:flush()
     local maxtries = 2
+    if item_type == "url" then
+      maxtries = 0
+    end
     tries = tries + 1
     if tries > maxtries then
       io.stdout:write(" Skipping.\n")
